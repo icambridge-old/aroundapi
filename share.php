34fdf9a71ed82129c18150eb31768c731b1acaee
Iain Cambridge <github@icambridge.me>
2013-09-21 17:09:08 +0100

    Remove EOL

--- share.php
+++ share.php
@@ -7,12 +7,11 @@ include_once('simple_html_dom.php');
 function scraping($url) {
 
     $html = file_get_html($url);
-    $time = (string) $html->find('tr[class="firstRow"] td',0 )->innertext.PHP_EOL;
 
-    $status = (string) $html->find('tr[class="firstRow"] td[class="status"]',0 )->innertext.PHP_EOL;
+    $time = (string) $html->find('tr[class="firstRow"] td',0 )->innertext;
+    $status = (string) $html->find('tr[class="firstRow"] td[class="status"]',0 )->innertext
 
     return compact("time", "status");	
-
 }
 
 
